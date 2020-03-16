f61711f44342f8ef6fafb266582d8e03ae358ed0
Chris Moe <chris.moe@gmail.com>
2020-03-16 12:04:22 -0700

    added bash script for second site found in port folder

--- port/build-port.sh
+++ port/build-port.sh
@@ -4,4 +4,4 @@ cat ./templates/top.html ./content/blog.html ./templates/bottom.html > ./docs/bl
 
 cat ./templates/top.html ./content/contact.html ./templates/bottom.html > ./docs/contact.html
 
-cat ./templates/top.html ./content/portfolio.html ./templates/bottom.html > ./docs/portfolio.html
+cat ./templates/top.html ./content/projects.html ./templates/bottom.html > ./docs/projects.html
