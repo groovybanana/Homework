73173588e522f68a040747256802c6b8304d1c19
Chris Moe <chris.moe@gmail.com>
2020-03-16 10:35:55 -0700

    completed bash script for first site

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
 cat ./templates/top.html ./content/index.html ./templates/bottom.html > ./docs/index.html
+
+cat ./templates/top.html ./content/about.html ./templates/bottom.html > ./docs/about.html
+
+cat ./templates/top.html ./content/blog.html ./templates/bottom.html > ./docs/blog.html
+
+cat ./templates/top.html ./content/contact.html ./templates/bottom.html > ./docs/contact.html
+
+cat ./templates/top.html ./content/portfolio.html ./templates/bottom.html > ./docs/portfolio.html
